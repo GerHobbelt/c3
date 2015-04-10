6e2315d558389bc6b7c7282b7010e1db101c2cd5
Matt Furness <matt.furness@gmail.com>
2015-04-10 13:48:08 +1000

    remove excess padding for y axes

--- c3.js
+++ c3.js
@@ -2594,7 +2594,7 @@
         } else if (!config.axis_y_show || config.axis_y_inner) { // && !config.axis_rotated
             return $$.axis.getYAxisLabelPosition().isOuter ? 30 : 1;
         } else {
-            return ceil10($$.getAxisWidthByAxisId('y', withoutRecompute));
+            return Math.ceil($$.getAxisWidthByAxisId('y', withoutRecompute));
         }
     };
     c3_chart_internal_fn.getCurrentPaddingRight = function () {
@@ -2607,7 +2607,7 @@
         } else if (!config.axis_y2_show || config.axis_y2_inner) { // && !config.axis_rotated
             return 2 + legendWidthOnRight + ($$.axis.getY2AxisLabelPosition().isOuter ? 20 : 0);
         } else {
-            return ceil10($$.getAxisWidthByAxisId('y2')) + legendWidthOnRight;
+            return Math.ceil($$.getAxisWidthByAxisId('y2')) + legendWidthOnRight;
         }
     };
     
@@ -2653,9 +2653,10 @@
     
     
     c3_chart_internal_fn.getAxisWidthByAxisId = function (id, withoutRecompute) {
-        var $$ = this;
-        return $$.axis.getMaxTickWidth(id, withoutRecompute);
+        var $$ = this, position = $$.axis.getLabelPositionById(id);
+        return $$.axis.getMaxTickWidth(id, withoutRecompute) + (position.isInner ? 10 : 20);
     };
+    
     c3_chart_internal_fn.getHorizontalAxisHeight = function (axisId) {
         var $$ = this, config = $$.config, h = 30;
         if (axisId === 'x' && !config.axis_x_show) { return 8; }
