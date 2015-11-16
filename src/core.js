6709454e008b23c428477de3962e4485c203d7af
Ger Hobbelt <ger@hobbelt.com>
2015-11-16 02:37:05 +0100

    Revert "Patched c3 core to put regions in forefront" -- this broke the data point tooltips in all examples!
    This reverts commit 57685851b7e989091a442404f2c24396a0ed4e51:
    Author: Andrew Appleby <andrewappleby22@gmail.com>
    Date:   Sun Aug 30 17:09:39 2015 -0400
    
        Patched c3 core to put regions in forefront
    
            This completely fixes my problem with cloning `rect`s and stuff, and takes a lot of stress off the app.
    
    > Reviewer @timmclean

--- src/core.js
+++ src/core.js
@@ -324,7 +324,10 @@ c3_chart_internal_fn.initWithData = function C3_INTERNAL_initWithData(data) {
     main.append("text")
         .attr("class", CLASS.text + ' ' + CLASS.empty)
         .attr("text-anchor", "middle") // horizontal centering of text at x position in all browsers.
-        .attr("dominant-baseline", "middle"); // vertical centering of text at y position in all browsers, except IE.    
+        .attr("dominant-baseline", "middle"); // vertical centering of text at y position in all browsers, except IE.
+
+    // Regions
+    $$.initRegion();
 
     // Grids
     $$.initGrid();
@@ -364,9 +367,6 @@ c3_chart_internal_fn.initWithData = function C3_INTERNAL_initWithData(data) {
 
     // Set targets
     $$.updateTargets($$.data.targets);
-
-    // Regions (explicitly put into the forefront for user-interaction)
-    $$.initRegion();
 
     // Draw with targets
     if (binding) {
