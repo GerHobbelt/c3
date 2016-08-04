2c454c1983d173c550be468ef9af1b420b568c30
Ger Hobbelt <ger@hobbelt.com>
2016-08-04 13:01:06 +0200

    load: *first* check, then access members. (prevents crash too!)

--- src/data.convert.js
+++ src/data.convert.js
@@ -9,10 +9,10 @@ c3_chart_internal_fn.convertUrlToData = function C3_INTERNAL_convertUrlToData(ur
     }
     req.get(function (error, data) {
         var d;
-        var dataResponse = data.response || data.responseText;
         if (!data) {
             throw new Error(error.responseURL + ' ' + error.status + ' (' + error.statusText + ')');
         }
+        var dataResponse = data.response || data.responseText;
         if (type === 'json') {
             d = $$.convertJsonToData(JSON.parse(dataResponse), keys);
         } else if (type === 'tsv') {
